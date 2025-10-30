e2f16be84a3012d5b245f7f40811321b207108af
itzjmbruhhh <reyesjohnmichael1029@gmail.com>
2025-10-30 18:54:01 +0800

    fixed file referencing for education

--- frontend/src/pages/Education.tsx
+++ frontend/src/pages/Education.tsx
@@ -34,33 +34,37 @@ function Education() {
           data-aos-duration="500"
           data-aos-delay="100"
         >
-          {educationData.map((educ: Education, index: number) => (
-            <div key={index} className="single-card box-shadow align-middle justify-center">
-              <div className="content-wrapper">
-                {/* Image Wrapper */}
-                <div className="image-wrapper mt-2">
-                  <img src={educ.imageSrc} alt="pic" className="image" />
-                </div>
+          {educationData.map((educ: Education, index: number) => {
+            // Dynamically import image
+            const imageSrc = new URL(`../assets/images/Education/${educ.imageSrc}`, import.meta.url).href;
 
-                {/* Info Wrapper */}
-                <div className="info-wrapper">
-                  <span className="span">{educ.level} || {educ.startYear}–{educ.endDate}</span>
+            return (
+              <div key={index} className="single-card box-shadow align-middle justify-center">
+                <div className="content-wrapper">
+                  {/* Image Wrapper */}
+                  <div className="image-wrapper mt-2">
+                    <img src={imageSrc} alt={educ.school} className="image" />
+                  </div>
 
-                  <h3 className="h3">{educ.school}</h3>
+                  {/* Info Wrapper */}
+                  <div className="info-wrapper">
+                    <span className="span">{educ.level} || {educ.startYear}–{educ.endDate}</span>
 
-                  <div>
-                    <p className="text-[16px] xl:text-xl">{educ.gpa}</p>
-                    {educ.achievements.map((achieve, i) => (
-                    <p key={i} className="text-[14px] xl:text-[16px] text-(--color-body-2)">
-                      - {achieve}
-                    </p>
+                    <h3 className="h3">{educ.school}</h3>
+
+                    <div>
+                      <p className="text-[16px] xl:text-xl">{educ.gpa}</p>
+                      {educ.achievements.map((achieve, i) => (
+                        <p key={i} className="text-[14px] xl:text-[16px] text-(--color-body-2)">
+                          - {achieve}
+                        </p>
                       ))}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
-
+            );
+          })}
         </div>
       </div>
     </section>
