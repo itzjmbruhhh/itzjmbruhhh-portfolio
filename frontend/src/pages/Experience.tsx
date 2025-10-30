266ff8657451bf7ba3716aaa36dc90752c177e0a
itzjmbruhhh <reyesjohnmichael1029@gmail.com>
2025-10-30 15:36:25 +0800

    added json for experience.tsx

--- frontend/src/pages/Experience.tsx
+++ frontend/src/pages/Experience.tsx
@@ -1,85 +1,59 @@
 import React from "react";
 import "../styles/Experience_Education.css";
-import Image from "../assets/images/home.jpg";
+import expData from "../../public/utils/Experience.json";
+
+interface Experience {
+  icon: string;
+  title: string;
+  bio: string;
+}
 
 function Experience() {
   return (
     <section id="experience" className="scroll-mt-[150px] xl:scroll-mt-[220px]">
       <div className="wrapper flex flex-col gap-10">
         {/* Section Heading */}
-        <div className="xl:mb-5" data-aos="fade-up" data-aos-anchor-placement="center-center" data-aos-duration="500">
+        <div
+          className="xl:mb-5"
+          data-aos="fade-up"
+          data-aos-anchor-placement="center-center"
+          data-aos-duration="500"
+        >
           <h3 className="bio">What I do?</h3>
           <h1 className="heading flex justify-center">My Experience</h1>
         </div>
 
         {/* Experience Cards Container */}
-        <div className="card-container" data-aos="fade-up" data-aos-anchor-placement="center-center" data-aos-duration="500" data-aos-delay="100">
+        <div
+          className="card-container"
+          data-aos="fade-up"
+          data-aos-anchor-placement="center-center"
+          data-aos-duration="500"
+          data-aos-delay="100"
+        >
+          
+          {/* Single Container */}
+          {expData.map((exp: Experience, index: number) => (
+            <div key={index} className="single-card-exp box-shadow">
+              <div className="content-wrapper">
+                <div className="info-wrapper-exp">
+                  <i
+                    className={`${exp.icon} text-(--color-primary) text-6xl`}
+                  ></i>
 
-          {/* Single Experience Card */}
-          <div className="single-card-exp box-shadow">
-            <div className="content-wrapper">
-              
+                  <h3 className="h3-exp">{exp.title}</h3>
 
-              {/* Info Wrapper */}
-              <div className="info-wrapper-exp">
-                <i className="las la-desktop text-(--color-primary) text-6xl"></i>
-
-                <h3 className="h3-exp">Frontend Development</h3>
-
-                <div>
-                  <p className="text-[16px] xl:text-xl">Lorem ipsum dolor.</p>
-                  <p className="text-[14px] xl:text-[18px] text-(--color-body-2)">
-                    Lorem ipsum dolor.
-                  </p>
+                  <div>
+                    <p className="text-[14px] xl:text-[18px] text-(--color-body-2) text-justify xl:mt-2">
+                      {exp.bio}
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          {/* End of Single Experience Card */}
+          ))}
+          {/* Single Container End */}
 
-          {/* Single Experience Card */}
-          <div className="single-card-exp box-shadow">
-            <div className="content-wrapper">
-              
-
-              {/* Info Wrapper */}
-              <div className="info-wrapper-exp">
-                <i className="las la-code text-(--color-primary) text-6xl"></i>
-
-                <h3 className="h3-exp">Backend Development</h3>
-
-                <div>
-                  <p className="text-[16px] xl:text-xl">Lorem ipsum dolor.</p>
-                  <p className="text-[14px] xl:text-[18px] text-(--color-body-2)">
-                    Lorem ipsum dolor.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* End of Single Experience Card */}
-
-          {/* Single Experience Card */}
-          <div className="single-card-exp box-shadow">
-            <div className="content-wrapper">
-              
-
-              {/* Info Wrapper */}
-              <div className="info-wrapper-exp">
-                <i className="las la-robot text-(--color-primary) text-6xl"></i>
-
-                <h3 className="h3-exp">Machine Learning</h3>
-
-                <div>
-                  <p className="text-[16px] xl:text-xl">Lorem ipsum dolor.</p>
-                  <p className="text-[14px] xl:text-[18px] text-(--color-body-2)">
-                    Lorem ipsum dolor.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* End of Single Experience Card */}
 
         </div>
       </div>
